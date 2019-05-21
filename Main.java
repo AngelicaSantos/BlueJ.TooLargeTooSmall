49207132f311233f5fd2073352de4f84c7da249f
Angelica Santos <angelicasantos@zipcoders-MacBook-Pro-2.local>
2019-05-21 18:46:43 -0400

    complete

--- Main.java
+++ Main.java
@@ -1,33 +1,39 @@
-
+import java.util.Scanner;
 /**
  * Write a description of class Main here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Angelica Santos)
+ * @version (5/21/19)
  */
 public class Main
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Main
-     */
-    public Main()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+{ 
+    public static void main(String[] args){ 
+        int counter=0;
+        int mysterynumber= 21;
+        int guess;
+        Scanner in = new Scanner(System.in); // declares scanner 
+        System.out.println("Hello, Welcome to my number guessing game.");//welcomes user to game
+        do{
+        System.out.println("Pick a number:"); // asks user to pick a number 
+        guess= in.nextInt(); // reads input and assigns 
+        counter++;
+        
+        if (guess >mysterynumber) System.out.println("guess is too large");
+        if (guess <mysterynumber) System.out.println("Guess is too small");
+        
+    } while (guess != mysterynumber);
+        System.out.println("Correct");
+        System.out.println(" It took " + counter + " tries ");
+        
     }
 }
+
+    
+    
+        
+    
+
+    
+    
+    
+
