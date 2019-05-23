c789979881ee60b112397124d58ea2b3cbd49bde
Angelica Santos <angelicasantos@zipcoders-MacBook-Pro-4.local>
2019-05-23 14:06:09 -0400

    changes made

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 /**
  * Write a description of class Main here.
  *
@@ -8,19 +9,35 @@ import java.util.Scanner;
 public class Main
 { 
     public static void main(String[] args){ 
+        Random randomNumber = new Random ();
+     
         int counter=0;
-        int mysterynumber= 21;
-        int guess;
+        int mysterynumber= randomNumber.nextInt(10);
+        int guess=0;
+        int previousNum=0;
+        
         Scanner in = new Scanner(System.in); // declares scanner 
         System.out.println("Hello, Welcome to my number guessing game.");//welcomes user to game
         do{
         System.out.println("Pick a number:"); // asks user to pick a number 
         guess= in.nextInt(); // reads input and assigns 
-        counter++;
         
-        if (guess >mysterynumber) System.out.println("guess is too large");
-        if (guess <mysterynumber) System.out.println("Guess is too small");
         
+        if (guess >mysterynumber){
+            System.out.println("guess is too large");
+        }
+        else if (guess <mysterynumber) {
+            System.out.println("Guess is too small");
+            }
+            if (guess != previousNum) {
+                counter++; 
+                previousNum=guess; 
+                
+            }
+           
+            
+            
+            
     } while (guess != mysterynumber);
         System.out.println("Correct");
         System.out.println(" It took " + counter + " tries ");
